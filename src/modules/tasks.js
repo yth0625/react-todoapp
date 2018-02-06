e15ff74f969605912165e38c0b267d3311f20d11
Taehyeon Yun <taehyeon.yun@architectgroup.com>
2018-02-06 13:48:58 +0900

    task state structure changem, Add Action and reducer

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -3,17 +3,19 @@ import { Map, List } from 'immutable';
 
 const CREATE = 'tasks/CREATE';
 const REMOVE = 'tasks/REMOVE';
+const CHECK = 'tasks/CHECK';
+const WRITETEXT = 'tasks/WRITETEXT'; 
 
 export const create = createAction(CREATE); // date, task(text, duedate)
 export const remove = createAction(REMOVE); // date, task(index)
+export const oncheck = createAction(CHECK); // index
+export const writetext = createAction(WRITETEXT);
 
 const initialState = Map({
     tasksList: List([
         Map({
+            date: '20180129',
             tasks: List([
-                Map({
-                    date: '20180129'
-                }),
                 Map({
                     check: false,
                     text: 'test',
@@ -27,10 +29,8 @@ const initialState = Map({
             ])
         }),
         Map({
+            date: '20180130',
             tasks: List([
-                Map({
-                    date: '20180130'
-                }),
                 Map({
                     check: false,
                     text: 'test',
@@ -42,6 +42,60 @@ const initialState = Map({
     selectDate: '20180129'
 });
 
+function setTask(state, action, changefn) { 
+    //state = state.setIn(['tasksList', action.payload.listIndex, 'tasks', action.payload.taskIndex, 'check'], true);
+    state = state.set('tasksList', state.get('tasksList').update(
+        action.payload.listIndex,
+        (tasksList) => {
+            const tasks = tasksList.get('tasks');
+            return tasksList.set('tasks', tasks.update(
+                action.payload.taskIndex,
+                changefn
+            ));
+        }
+    ));
+    return state;
+}
 
 export default handleActions({
+    [CREATE]: (state, action) => {
+        state = state.set('tasksList', state.get('tasksList').update(
+            action.payload,
+            (tasksList) => tasksList.set('tasks', tasksList.get('tasks').push(
+                Map({
+                    check: false,
+                    text: '',
+                    duedate: ''
+                })
+            ))
+        ));
+        return state;
+    },
+
+    [REMOVE]: (state, action) => {
+        state = state.set('tasksList', state.get('tasksList').update(
+            action.payload.listIndex,
+            (tasksList) => tasksList.set('tasks', tasksList.get('tasks').delete(action.payload.taskIndex))
+        ));
+        return state;
+    },
+
+    [CHECK]: (state, action) => {
+        const changefn = (task) => {
+            return task.set('check', !task.get('check'));
+        }
+        state = setTask(state, action, changefn);
+        return state;
+    },
+
+    [WRITETEXT]: (state, action) => {
+        const changefn = (task) => {
+            return task.set('text', action.payload.e.target.value);
+        }
+        state = setTask(state, action, changefn);
+
+        console.log(action.payload.e);
+
+        return state;
+    }
 }, initialState);
