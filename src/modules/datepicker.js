cc679f16ce30e7ea72c94d2f736932b7fe6e66e2
yth0625 <yth0625@gmail.com>
2018-02-09 14:19:14 +0900

    Added datepicker Docks moudle

---
+++ src/modules/datepicker.js
@@ -0,0 +1,17 @@
+import { handleActions, createAction } from 'redux-actions';
+import { Map } from 'immutable';
+
+const SELECTDATE = 'datepicker/SELCETDATE';
+
+export const selectdate = createAction(SELECTDATE);
+
+const initialState = Map({
+    selectDate: new Date(2018, 0, 29)
+});
+
+export default  handleActions({
+    [SELECTDATE]: (state, action) => {
+        state = state.set('selectDate', action.payload);
+        return state;
+    }
+}, initialState);
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import tasks from './tasks.js';
+import datepicker from './datepicker';
 
 export default combineReducers({
-    taskStorage: tasks
+    taskStorage: tasks,
+    datePickerStorage: datepicker
 });
