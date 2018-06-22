55fff93ecf999b2653b791dd37e66202d74bdb96
yth0625 <yth0625@gmail.com>
2018-06-22 17:52:44 +0900

    Removed redux-promise-middleware

--- todo-frontend/src/store.js
+++ todo-frontend/src/store.js
@@ -3,10 +3,9 @@ import modules from './modules';
 
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
-import promiseMiddleware from 'redux-promise-middleware';
 
 const logger = createLogger();
 
-const store = createStore(modules, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(logger, ReduxThunk, promiseMiddleware()));
+const store = createStore(modules, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(logger, ReduxThunk));
 
 export default store;
