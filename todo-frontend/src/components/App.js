abef120759cc66403f4e800b4a6203f42c19accf
yth0625 <yth0625@gmail.com>
2018-10-26 15:08:52 +0900

    Added react-router

--- todo-frontend/src/components/App.js
+++ todo-frontend/src/components/App.js
@@ -1,19 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import TaskListContainer from '../containers/TaskListContainer';
 import DataPickerContainer from '../containers/DatePickerContainer';
+import LoginFormContainer from '../containers/LoginFormContainer';
+import SignUpContainer from '../containers/SignUpContainer';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import { getCookie, browserHistory } from '../utills/Utill';
+
+const Todo = ({ match }) => (
+    <div>
+        <MuiThemeProvider>
+            <DataPickerContainer
+                parameterString = {match.params.date}
+            />
+        </MuiThemeProvider>
+        <MuiThemeProvider>
+            <TaskListContainer/>
+        </MuiThemeProvider>
+    </div>
+);
+
+Todo.propTypes = {
+    match: PropTypes.object
+};
+
+const Login = () => {
+    return (
+        <div>
+            <MuiThemeProvider>
+                <LoginFormContainer/>
+            </MuiThemeProvider>
+        </div>
+    )
+}
+
+const SignUp = () => {
+    return (
+        <div>
+            <SignUpContainer/>
+        </div>
+    )
+}
 
 const App = () => {
     return (
         <div>
-            <MuiThemeProvider>
-                <DataPickerContainer/>
-            </MuiThemeProvider>
-            <MuiThemeProvider>
-                <TaskListContainer/>
-            </MuiThemeProvider>
+            <Route history={browserHistory}>
+                <Switch>
+                    <Route path="/todo/:date" component={Todo}/>
+
+                    <Route path ="/todo" component={Todo}/>
+
+                    <Route path="/login" component={Login}/>
+
+                    <Route path="/signup" component={SignUp}/>
+    
+                    <Route path="/" render={() => (
+                        getCookie('userId') ? <Redirect to="/todo"/>
+                            : <Redirect to="/login"/>)}/>
+                </Switch>
+            </Route>
         </div>
     );
 }; 
--- todo-frontend/src/index.js
+++ todo-frontend/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { browserHistory } from './utills/Utill';
+
 import store from './store';
 import './style/style.css';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <Router history = {browserHistory}>
+            <App/>
+        </Router>
     </Provider>, 
     document.getElementById('root'));
 registerServiceWorker();
